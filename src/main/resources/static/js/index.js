4e5f7ceafbbf8424ec749282cbe1a8801cefd792
Ayrat <ayrat1@mail.ru>
2020-10-26 14:54:34 +0300

    chapter 1 complete

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,17 +1,30 @@
-webix.ready( function() {
-    webix.ui({
-        type: "line",
-        container: "app",
-        id: 'root',
-        height: 400,
-        rows: [
-            { template: "Row 1" },
-            { template: "Row 2"},
-            { cols: [
-                { template: "Col 1" },
-                { template: "Col 2"}
-                ]
-            }
-        ]
-    });
+
+requirejs.config({
+    baseUrl: 'js'
+})
+
+
+function buildRoute(view) {
+    return function() {
+        webix.ui({
+            id: 'root',
+            rows: [
+                view
+            ]
+        }, $$("root"))
+    }
+}
+
+require(['views/main', 'views/cars'], function(main, cars) {
+    webix.ready(function() {
+        webix.ui({
+            id: 'root',
+            container: "app"
+        })
+    })
+
+    routie({
+        '': buildRoute(main),
+        'cars': buildRoute(cars)
+    })
 })
