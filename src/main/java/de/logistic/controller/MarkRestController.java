84644e10c92a5493253b8c740f9172eaaae06bd2
Ayrat <ayrat1@mail.ru>
2020-10-26 21:52:08 +0300

    lesson 2 complete. Add REST api

---
+++ src/main/java/de/logistic/controller/MarkRestController.java
@@ -0,0 +1,14 @@
+package de.logistic.controller;
+
+import de.logistic.domain.Mark;
+import de.logistic.repo.MarkRepo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/mark")
+public class MarkRestController extends AbstractRestController<Mark, MarkRepo> {
+    public MarkRestController(MarkRepo repo) {
+        super(repo);
+    }
+}
