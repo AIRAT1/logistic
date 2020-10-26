5c97ad90e306000090525456c557fb80362d6319
Ayrat <ayrat1@mail.ru>
2020-10-26 22:58:54 +0300

    course complete

--- src/main/java/de/logistic/domain/Model.java
+++ src/main/java/de/logistic/domain/Model.java
@@ -2,6 +2,7 @@ package de.logistic.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import de.logistic.util.EntityIdResolver;
 import lombok.Data;
@@ -25,4 +26,10 @@ public class Model implements ComboListItem {
     @ManyToOne
     @JsonIdentityReference(alwaysAsId = true)
     private Mark mark;
+
+    @Override
+    @JsonIgnore
+    public String getRepr() {
+        return String.format("%s %s", mark.getName(), name);
+    }
 }
